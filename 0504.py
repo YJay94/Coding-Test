3470c29ff3407ebcd2931d3310a80f8cf210d3a2
YJay94 <83999253+YJay94@users.noreply.github.com>
2022-05-04 18:02:05 +0900

    Add files via upload

--- 0504.py
+++ 0504.py
@@ -15,3 +15,26 @@ def solution(scoville, K):
         heapq.heappush(scoville,a+b*2)
         i = i + 1
         answer = i
+
+
+
+--------------------------------
+int()
+bin()
+oct()
+hex()
+--------------------------------
+
+list(str) <= 문자열1글자식 자르기
+
+
+# re모듈
+re.match("one", string)
+findall()
+match()
+split()
+
+
+
+string.find('a')
+string.index('a')
